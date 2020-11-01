8b8bda283d1d5098e4e4b9f0c8116a1631b6845b
Katie Munoz <katyv8286@gmail.com>
2020-10-31 23:39:42 -0400

    updated css and jquery

--- Assets/script.js
+++ Assets/script.js
@@ -1,16 +1,16 @@
-//set moment to a variable
+//Created a variable for moment
 var now = moment();
-//create a variable for the current date
+//Created a variable for current date
 var currentDate = now.format("MM DD YYYY");
 
-// Set date 
+// Set the current date 
 $("#currentDay").text("Today's Date: " + currentDate);
 
 
 
 $(document).ready(function() {
 
-    // For loop to get and display tasks from local storage
+    // Created a for loop to get and display items from local storage
     hourArr = $('.hour').toArray();
     for (i = 0; i < hourArr.length; i++) {
         $(hourArr[i]).siblings('textarea').text(localStorage.getItem($(hourArr[i]).attr('data-time')));
@@ -18,19 +18,19 @@ $(document).ready(function() {
 });
 
 
-// For loop to print rows with timeblocks, taskblocks, and save buttons
+// Created a for loop for rows with timeblocks, taskblocks, and save buttons
 for (i = 0; i < 9; i++) {
-    //create a variable for the row
+    //created a variable for the row
     var rowBlock = $('<div>').addClass('row');
-    //create a variable for the time block
+    //created a variable for the time block
     var timeBlock = $('<div>').addClass('hour col-md-2').text(moment('9:00 AM', 'hh:mm A').add(i, 'hours').format('hA'));
     timeBlock.attr('data-time', moment('9:00 AM', 'hh:mm A').add(i, 'hours').format('hA'));
-    //create a variable for the taskblock
+    //created a variable for the taskblock
     var taskBlock = $('<textarea>').addClass('col-md-9');
-    //create a variable for the save block
+    //created a variable for the save block
     var saveButton = $('<button>').addClass('saveBtn col-md-1').html('<i class="fas fa-save"></i>');
 
-    // Placing content created above into the DOM in the right order.
+    
     //append the container with the row
     $('.container').append(rowBlock);
     //append the row with the time block
@@ -41,20 +41,20 @@ for (i = 0; i < 9; i++) {
     $(taskBlock).after(saveButton);
 
 
-    // if else statement to determine the color of the row
-    //if the time is the same as the time on the timeblock, display task block as red
+    // Created if else statement to determine the color of the row
+    //if the time is the same as the time on the timeblock, display task block in red color
     if (now.isSame(moment('9:00 AM', 'hh:mm A').add(i, 'hours'), 'hour')) {
         $(taskBlock).addClass('present');
-        //if the time is in the future, display green
+        //if the time is in the future, display in green color
     } else if (now.isBefore(moment('9:00 AM', 'hh:mm A').add(i, 'hours'), 'hour')) {
         $(taskBlock).addClass('future');
-        //if the time is in the past, display grey
+        //if the time is in the past, display in grey color
     } else if (now.isAfter(moment('9:00 AM', 'hh:mm A').add(i, 'hours'), 'hour')) {
         $(taskBlock).addClass('past');
     }
 }
 
-// Save click event to store data in local storage
+// Save click events to be able to store data in local storage
 $('.saveBtn').on('click', function() {
 
     localStorage.setItem($(this).siblings('div.hour').attr('data-time'), $(this).siblings('textarea').val())
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # ThirdPartyAPIs
+
+Due Date: Oct. 31st, 2020
+Author: Katie Munoz
+
+Creating a 9:00am - 5:00pm work day planner, with HTML, CSS and JQuery.
+Referred to www.momentjs.com library to set up the date and time as per the given instructions.
